7b0f1e0e71c30b6fe4fd3eb7f75e99e4ffbe6505
Ricardo <ricardo_hentges@hotmail.com>
2023-03-25 01:18:02 -0400

    melhorada a saída no console

---
+++ Exercicio_1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PontoCartesiano ponto1 = new PontoCartesiano(1, 2);
+        PontoCartesiano ponto2 = new PontoCartesiano(1, 4);
+
+        PlanoCartesiano pc = new PlanoCartesiano();
+        pc.setP1(ponto1);
+        pc.setP2(ponto2);
+
+        System.out.println("Distância: " + pc.calcularDistanciaEuclidiana());
+    }
+    
+}
