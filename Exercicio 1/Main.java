ec132b8c0af578f9f7f33dced569709c36019a75
Ricardo <ricardo_hentges@hotmail.com>
2023-03-24 21:47:58 -0400

    solução final

--- Exercicio 1/Main.java
+++ Exercicio 1/Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) {
         PontoCartesiano ponto1 = new PontoCartesiano(1, 2);
         PontoCartesiano ponto2 = new PontoCartesiano(1, 4);
-
-        System.out.println(PlanoCartesiano.calcularDistanciaEuclidiana(ponto1, ponto2));
+        PlanoCartesiano pc = new PlanoCartesiano();
+        pc.setP1(ponto1);
+        pc.setP2(ponto2);
+        System.out.println(pc.calcularDistanciaEuclidiana());
     }
     
 }
--- Exercicio 1/PlanoCartesiano.java
+++ Exercicio 1/PlanoCartesiano.java
@@ -1,10 +1,28 @@
 public class PlanoCartesiano {
 
-    public static double calcularDistanciaEuclidiana(PontoCartesiano x, PontoCartesiano y) {
-        final double distanciaX = Math.pow(x.getX() - y.getX(), 2);
-        final double distanciaY = Math.pow(x.getY() - y.getY(), 2);
+    PontoCartesiano p1;
+    PontoCartesiano p2;
+
+    public double calcularDistanciaEuclidiana() {
+        final double distanciaX = Math.pow(p1.getX() - p1.getX(), 2);
+        final double distanciaY = Math.pow(p2.getY() - p2.getY(), 2);
 
         return Math.sqrt(distanciaX + distanciaY);
     }
+
+    public PontoCartesiano getP1() {
+        return p1;
+    }
+    public void setP1(PontoCartesiano p1) {
+        this.p1 = p1;
+    }
+
+    public PontoCartesiano getP2() {
+        return p2;
+    }
+    public void setP2(PontoCartesiano p2) {
+        this.p2 = p2;
+    }
+
     
 }
