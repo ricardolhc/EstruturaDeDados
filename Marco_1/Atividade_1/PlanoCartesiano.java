7fbb73c7da30a2548f3e6e8f5f0ac3c08121067b
Ricardo <ricardo_hentges@hotmail.com>
2023-03-26 20:55:12 -0400

    Melhor organização do repositório

---
+++ Marco_1/Atividade_1/PlanoCartesiano.java
@@ -0,0 +1,40 @@
+public class PlanoCartesiano {
+
+    private PontoCartesiano p1;
+    private PontoCartesiano p2;
+
+    public PlanoCartesiano() {
+        p1 = new PontoCartesiano(0, 0);
+        p2 = new PontoCartesiano(0, 0);
+    }
+
+    public PlanoCartesiano(PontoCartesiano p1, PontoCartesiano p2) {
+        this.p1 = p1;
+        this.p2 = p2;        
+    }
+
+    public PontoCartesiano getP1() { 
+        return p1; 
+    }
+
+    public PontoCartesiano getP2() { 
+        return p2; 
+    }
+
+    public void setP1(PontoCartesiano p1) { 
+        this.p1 = p1; 
+    }
+
+    public void setP2(PontoCartesiano p2) { 
+        this.p2 = p2; 
+    }
+
+    public double calcularDistanciaEuclidiana() {
+        final double distanciaX = Math.pow(p1.getX() - p2.getX(), 2);
+        final double distanciaY = Math.pow(p1.getY() - p2.getY(), 2);
+
+        final double distancia = Math.sqrt(distanciaX + distanciaY);
+
+        return distancia;
+    }
+}
