5ad9f69e09248c058ff7601e1cdcbf5b9e0cdb9c
Ricardo <ricardo_hentges@hotmail.com>
2023-04-04 18:31:30 -0400

    solução atividade 3

---
+++ Marco_1/Atividade_3/IVetor.java
@@ -0,0 +1,11 @@
+package Marco_1.Atividade_3;
+
+public interface IVetor {
+    
+    public void adiciona(Aluno aluno);
+    public int getTamanho();
+    public boolean contem(Aluno aluno);
+    public boolean cheio();
+    public boolean remove(Aluno aluno);
+
+}
