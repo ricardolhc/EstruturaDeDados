79e18986cc71ace5a5775d810d005d1a6e036f8a
Ricardo Costa <ricardo_hentges@hotmail.com>
2023-05-20 14:53:37 -0300

    complexidade adicionada no código

--- artifact/src/main/java/com/group/Marco_1/ProvaPratica_1/Questao_1/Main.java
+++ artifact/src/main/java/com/group/Marco_1/ProvaPratica_1/Questao_1/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 // Questão 1) Escreva um programa iterativo (Java) que recebe como entrada dois arranjos (dois vetores de inteiros), 
 // cada um com n elementos, e devolve como saída a interseção entre os dois. O retorno é outro vetor contendo os elementos 
 // comuns a ambos os vetores. Faça a análise de complexidade de seu algoritmo no pior caso e relate no seu PIÁ. 
+// 5n^2 + 12n + 12
+
 
 public class Main {
 
